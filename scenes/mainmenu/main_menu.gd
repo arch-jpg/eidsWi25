1fc724f87a7d446c659dbe076920d96031b367c7
arch-jpg <ohans820@gmail.com>
2025-11-11 13:55:00 +0100

    added scenes folder

--- scenes/mainmenu/main_menu.gd
+++ scenes/mainmenu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_PLAY_pressed() -> void:
 	print("start pressed") 
-	get_tree().change_scene_to_file("res://playermenu/player_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/playermenu/player_menu.tscn")
 
 
 func _on_OPTIONS_pressed() -> void:
