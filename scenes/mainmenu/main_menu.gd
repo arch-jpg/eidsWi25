0a6f90643108d32487821ec3ff99dbbdaab6f330
arch-jpg <ohans820@gmail.com>
2025-11-17 12:03:01 +0100

    added more changes to player menu (#5)

--- scenes/mainmenu/main_menu.gd
+++ scenes/mainmenu/main_menu.gd
@@ -1,14 +1,25 @@
 extends Control
 
+var button_type = null
 
 func _on_PLAY_pressed() -> void:
+	button_type="start"
+	$fadeout.show()
+	$fadeout/fade_timer.start()
+	$fadeout/AnimationPlayer.play("fade_out")
 	print("start pressed") 
-	get_tree().change_scene_to_file("res://scenes/playermenu/player_menu.tscn")
 
 
 func _on_OPTIONS_pressed() -> void:
-	print("options pressed") 
+	button_type="options"
 
 
 func _on_EXIT_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_fade_timer_timeout() -> void:
+	if button_type == "start":
+		get_tree().change_scene_to_file("res://scenes/playermenu/player_menu.tscn")
+	elif button_type == "options":
+		print("optiions pressed")
--- scenes/playermenu/player_menu.gd
+++ scenes/playermenu/player_menu.gd
@@ -1,5 +1,19 @@
 extends Control
 
+func _ready() -> void:
+	$fadeout.show()
+	$fadeout/AnimationPlayer.play("fade_in")
+	
 
-func _on_TEST_pressed() -> void:
-	print("Test passed")
+
+
+func _on_b_deck_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_b_shop_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_b_start_pressed() -> void:
+	pass # Replace with function body.
