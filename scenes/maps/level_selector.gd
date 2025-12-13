18be67313e1e879ce3d3598f3d6ebe0e6c3f7ef8
PSGMaTeNa <lilge.marten@gmail.com>
2025-12-13 16:34:42 +0100

    preset map + asset. .save data name from deck to player.save

---
+++ scenes/maps/level_selector.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx:int) -> void:
+
+    if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+        pass # Replace with function body.
+    else:
+        pass # Replace with function body.
