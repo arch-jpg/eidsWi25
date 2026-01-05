e11ebc0d4e74c9c87d5960da5cec4a63c7affaf0
Karl <ele.vanqz@gmail.com>
2026-01-05 17:31:06 +0100

    Added Unlock System, card count, comabat szene background #21, #15

--- scenes/playermenu/player_menu.gd
+++ scenes/playermenu/player_menu.gd
@@ -2,8 +2,9 @@ extends Control
 
 
 func _on_kampf_pressed() -> void:
-	print("Kampf button pressed - TODO: Implement battle scene")
+	print("Kampf button pressed - Map for Path Figth")
 	# TODO: get_tree().change_scene_to_file("res://scenes/battle/battle.tscn")
+	get_tree().change_scene_to_file("res://scenes/maps/map_01.tscn")
 
 
 func _on_deck_editor_pressed() -> void:
@@ -14,5 +15,3 @@ func _on_deck_editor_pressed() -> void:
 func _on_back_pressed() -> void:
 	print("Back to main menu")
 	get_tree().change_scene_to_file("res://scenes/mainmenu/main_menu.tscn")
-
-
