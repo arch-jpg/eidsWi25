b9fed62e8d56ec811fb12f96b4c5253c054657dc
PSGMaTeNa <lilge.marten@gmail.com>
2026-01-18 15:00:44 +0100

    Energy count can rech 9 and stays as max

--- scenes/fightscene/combat_scene.gd
+++ scenes/fightscene/combat_scene.gd
@@ -223,7 +223,9 @@ func _take_enemy_turn(id: String):
 	pass
 
 func _on_end_turn_btn_pressed() -> void:
-	energy_count=4
+	energy_count +=4
+	if energy_count >9:
+		energy_count=9
 	$Label.text=str(energy_count)
 	$handcontainer.end_turn()
 	draw_hand(frontpointer)
