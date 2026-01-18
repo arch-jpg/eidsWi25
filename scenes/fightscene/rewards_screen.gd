882a64997b67ffd697a43616aa1d3e0c2131f022
Karl <ele.vanqz@gmail.com>
2026-01-18 22:12:31 +0100

    deletet status effect karts, changed some values, prettier reward screen

--- scenes/fightscene/rewards_screen.gd
+++ scenes/fightscene/rewards_screen.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 
 func _on_exit_pressed() -> void:
-	var gold_amount: int = int(get_child(3).text)
+	var gold_label = $CenterContainer/MainPanel/VBoxContainer/GoldPanel/GoldContainer.get_node("gold amount")
+	var gold_amount: int = int(gold_label.text)
 	GameState.add_gold(gold_amount)
-	print(get_child(3).text)
+	print("Gold earned: ", gold_amount)
 	get_tree().change_scene_to_file("res://scenes/map_system/map_screen.tscn")
-	pass # Replace with function body.
